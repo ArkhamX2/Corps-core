9c5e8a0e6f75258452a471564e245d1aa38c47d8
vadimyt <pvcs@yandex.ru>
2024-05-31 14:00:09 +0300

    fix

--- MegaCorps/Corps.Server/Hubs/GameHub.cs
+++ MegaCorps/Corps.Server/Hubs/GameHub.cs
@@ -65,8 +65,7 @@ namespace Corps.Server.Hubs
 
                 await Groups.AddToGroupAsync(Context.ConnectionId, joinTo.Id + "Player");
                 await Clients.Group(joinTo.Id + "Player").SendAsync("JoinSuccess", joinTo, playerId);
-                foreach (var lobbyMembers in joinTo.lobbyMembers)
-                    await Clients.Group(joinTo.Id + "Player").SendAsync("PlayerJoined", joinTo);
+                await Clients.Group(joinTo.Id + "Player").SendAsync("PlayerJoined", joinTo);
                 await Clients.Group(joinTo.Id + "Host").SendAsync("PlayerJoined", joinTo);
 
                 Log_Lobby(nameof(JoinLobby));
@@ -93,8 +92,7 @@ namespace Corps.Server.Hubs
                 joinTo.PlayerReadyChange(playerId);
 
                 await Clients.Group(lobbyId + "Player").SendAsync("ReadySuccess");
-                foreach (var lobbyMembers in joinTo.lobbyMembers)
-                    await Clients.Group(joinTo.Id + "Player").SendAsync("LobbyMemberReady", joinTo);
+                await Clients.Group(lobbyId + "Player").SendAsync("LobbyMemberReady", joinTo);
                 await Clients.Group(lobbyId + "Host").SendAsync("LobbyMemberReady", _lobbies[lobbyId]);
 
                 Log_Lobby(nameof(LobbyMemberReady));
